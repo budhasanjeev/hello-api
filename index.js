a0710668321b750a2c8e3b81204f4317ab656ebd
budhasanjeev <budhasanjeev@gmail.com>
2022-01-05 14:08:15 +0900

    Third commit

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ app.post("/approvalSync4", (req, res, next) => {
   }, 10000)
 });
 
-app.listen(3000, (err) => {
-  if (err) throw err
-  console.log('Server running in http://127.0.0.1:3000')
+app.listen(process.env.PORT || 3000, (err) => {
+  console.log('server running on port 3000', '');
 });
