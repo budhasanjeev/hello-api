63019c5e327d3b27bfca2182d5ca19b9f701bfd6
budhasanjeev <budhasanjeev@gmail.com>
2022-01-05 13:55:47 +0900

    second commit

--- index.js
+++ index.js
@@ -5,27 +5,31 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("Hello API!");
+});
+
 app.post("/approvalSync1", (req, res, next) => {
   setTimeout(() => {
-    res.json("I am wake!!");
+    res.json("I am wake!! after 100ms ms");
   }, 100)
 });
 
 app.post("/approvalSync2", (req, res, next) => {
   setTimeout(() => {
-    res.json("I am wake!!");
+    res.json("I am wake!! after 1000 ms");
   }, 1000)
 });
 
-app.post("/approvalSync2", (req, res, next) => {
+app.post("/approvalSync3", (req, res, next) => {
   setTimeout(() => {
-    res.json("I am wake!!");
+    res.json("I am wake!! after 2000 ms");
   }, 2000)
 });
 
-app.post("/approvalSync2", (req, res, next) => {
+app.post("/approvalSync4", (req, res, next) => {
   setTimeout(() => {
-    res.json("I am wake!!");
+    res.json("I am wake!! after 10000 ms");
   }, 10000)
 });
 
